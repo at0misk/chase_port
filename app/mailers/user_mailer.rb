9e89a892950891bc6c34a63f87b4f5467208c287
Kevin Taylor <kevintaylor@Kevins-MacBook-Pro.local>
2017-03-04 15:28:43 -0800

    updated mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
  
   def email(first, last, address, subjectP, message)
     @url  = 'http://example.com/login'
-    mail(to: 'ktp925@gmail.com', subject: "TEST", body: first + " " + last + " " + address + " " + message)
+    mail(to: 'ktp925@gmail.com', subject: "Website Mailer", body: "First: " + first + " " + "Last: " + last + " " + "Email: " + address + " " + "Subject: " + subjectP + " " + "Message: " + " " + message)
   end
 
 end
